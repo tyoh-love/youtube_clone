42cae2caae0fcb30512e0bb3e62e06bf10efb399
mhyongoh@gmail.com <{your_email}>
2024-12-12 21:28:07 +0900

    demo update

--- src/utils/contstant.tsx
+++ src/utils/contstant.tsx
@@ -15,8 +15,8 @@ import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
-  { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
+  { name: "홈", icon: <HomeIcon /> },
+  { name: "리엑트", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
